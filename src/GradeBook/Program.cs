e4fe3c37dfc657ca6ba9715418720ce65fa7304a
awpala <adam.w.pala@gmail.com>
2021-05-01 16:35:58 -0500

    Refactor method `Main()` to console inputs

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -4,16 +4,25 @@ namespace GradeBook {
   class Program {
     static void Main(string[] args) {
       var book = new Book("Scott's Grade Book");
-      book.AddGrade(89.1);
-      book.AddGrade(90.5);
-      book.AddGrade(77.5);
+
+      while(true) {
+        Console.WriteLine("Enter a grade or `a` to quit");
+        var input = Console.ReadLine();
+
+        if(input.ToLower() == "q") {
+          break;
+        }
+
+        var grade = double.Parse(input);
+        book.AddGrade(grade);
+      }
 
       var stats = book.GetStatistics();
 
       Console.WriteLine($"The lowest grade is {stats.LowestGrade}");
       Console.WriteLine($"The highest grade is {stats.HighestGrade}");
       Console.WriteLine($"The average grade is {stats.Average:N1}");
-      Console.WriteLine($"The letter grade is {stats.Letter}");
+      Console.WriteLine($"The average letter grade is {stats.Letter}");
     }
   }
 }
