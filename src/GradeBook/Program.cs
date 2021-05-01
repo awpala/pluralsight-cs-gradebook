b8e8314d266cfafa6af02833b5312c3c9efb9c6c
awpala <adam.w.pala@gmail.com>
2021-05-01 12:52:03 -0500

    Add initial code

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GradeBook
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var name = (args.Length == 1) ? args[0] : "there";
+      Console.WriteLine($"Hello {name}!");
+    }
+  }
+}
